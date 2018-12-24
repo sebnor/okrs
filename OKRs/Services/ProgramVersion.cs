3bb132a7db799930ab42b91607eb20a101b8b158
Sebastian Nordin <sebastian.nordin@ifacts.se>
2018-12-24 21:50:37 +0100

    Add program version

---
+++ OKRs/Services/ProgramVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace OKRs.Services
+{
+    public class ProgramVersion
+    {
+        private readonly OKRsConfiguration configuration;
+
+        public ProgramVersion(IOptions<OKRsConfiguration> configuration)
+        {
+            this.configuration = configuration.Value;
+        }
+        public string Version()
+        {
+            return configuration.ProgramVersion;
+        }
+    }
+}
